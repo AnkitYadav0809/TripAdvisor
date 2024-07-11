f644938aeadd2603cd2e1c8e5e86de56be7ac0d6
Ankit Yadav <ankityadav60062@gmail.com>
2024-07-11 13:43:03 +0530

    changed

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
-import React from 'react'
-import data from './data'
-import { useState } from 'react'
+import React, { useState } from "react";
+import data from './data.js';
+import Tours from './Component/Tours';
 
 const App = () => {
-  const[tours , setTours]= useState(data)
+  const [tours, setTours] = useState(data);
+  
+  function removeTour(id) {
+    const newTours = tours.filter(tour => tour.id !== id);
+    setTours(newTours);
+  }
+
+  if (tours.length === 0) {
+    return (
+      <div className="refresh">
+        <h2>No Tours Left</h2>
+        <button className="btnWhite" onClick={() => setTours(data)}>Refresh</button>
+      </div>
+    );
+  }
+
   return (
-    <div>
-      <h2>TOUR TO INDIA</h2>
-     <Tours> </Tours>
+    <div className="app">
+      <Tours tours={tours} removeTours={removeTour} />
     </div>
   )
-}
 
-export default App
+};
+
+export default App;
